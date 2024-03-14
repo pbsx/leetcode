d469e96639cbd468a7c5ff87f9c90669e84a343c
pbsx <163014538+pbsx@users.noreply.github.com>
2024-03-14 08:20:03 +0530

    Time: 72 ms (55.83%) | Memory: 55.6 MB (40.49%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.js
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.js
@@ -4,17 +4,17 @@
  * @return {number}
  */
 var numSubarraysWithSum = function (nums, goal) {
-    let count = 0;
-    let sum = 0;
-    let prefixSum = { 0: 1 };
+    let sum = 0
+    let count = 0
+    let prefixSum = { 0: 1 }
 
     for (let num of nums) {
-        sum += num;
+        sum += num
         if (prefixSum[sum - goal]) {
-            count += prefixSum[sum - goal];
+            count += prefixSum[sum - goal]
         }
-        prefixSum[sum] = (prefixSum[sum] || 0) + 1;
+        prefixSum[sum] = (prefixSum[sum] || 0) + 1
     }
 
-    return count;
-};
+    return count
+}
