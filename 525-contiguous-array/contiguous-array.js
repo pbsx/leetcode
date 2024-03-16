7a9044529d96c2df0f92d30d80ca6c49e883b032
pbsx <163014538+pbsx@users.noreply.github.com>
2024-03-16 10:50:03 +0530

    Time: 103 ms (40.69%) | Memory: 58.8 MB (30.30%) - LeetSync

---
+++ 525-contiguous-array/contiguous-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxLength = function (nums) {
+    let prefixSum = {}
+    let sum = 0
+    let max = 0
+    nums.forEach((num, index) => {
+        sum = num === 0 ? sum - 1 : sum + 1
+        if (sum === 0) {
+            max = index + 1
+        }
+        if (sum in prefixSum) {
+            max = Math.max(index - prefixSum[sum], max)
+        } else {
+            prefixSum[sum] = index
+        }
+    })
+
+    return max
+};
