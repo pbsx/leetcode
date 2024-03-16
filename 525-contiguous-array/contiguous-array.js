90b87793f69637d5742544d837e899cd7b74d2fb
pbsx <163014538+pbsx@users.noreply.github.com>
2024-03-16 10:51:18 +0530

    Time: 116 ms (26.62%) | Memory: 58.8 MB (27.70%) - LeetSync

--- 525-contiguous-array/contiguous-array.js
+++ 525-contiguous-array/contiguous-array.js
@@ -3,14 +3,11 @@
  * @return {number}
  */
 var findMaxLength = function (nums) {
-    let prefixSum = {}
+    let prefixSum = { 0: -1 }
     let sum = 0
     let max = 0
     nums.forEach((num, index) => {
         sum = num === 0 ? sum - 1 : sum + 1
-        if (sum === 0) {
-            max = index + 1
-        }
         if (sum in prefixSum) {
             max = Math.max(index - prefixSum[sum], max)
         } else {
