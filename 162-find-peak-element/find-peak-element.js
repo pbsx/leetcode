da65d48ca7a80c48fdb0320a55e4aa032869a67a
pbsx <163014538+pbsx@users.noreply.github.com>
2024-03-18 21:59:50 +0530

    Time: 48 ms (75.28%) | Memory: 51.1 MB (5.14%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function (nums) {
+    let right = nums.length - 1
+    let left = 0
+    if (left === right) {
+        return left
+    }
+    while (left <= right) {
+        let length = right - left + 1
+        if (length === 1) {
+            return left
+        }
+        let mid = Math.floor(left + (right - left + 1) / 2)
+        console.log(left, right, mid)
+
+        switch (mid) {
+            case left: return nums[left] > nums[left + 1] ? left : left + 1
+            case right: return nums[right] > nums[right - 1] ? right : right - 1
+            default: {
+                if ((nums[mid] > nums[mid - 1]) && (nums[mid] > nums[mid + 1])) {
+                    return mid
+                }
+                if (nums[mid] < nums[mid + 1]) {
+                    left = mid + 1
+                } else {
+                    right = mid
+                }
+            }
+        }
+        console.log(left, right, mid)
+    }
+};
