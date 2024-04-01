199a58a269aeb429e46381a8d34e2d44956948aa
pbsx <163014538+pbsx@users.noreply.github.com>
2024-04-01 19:15:06 +0530

    Time: 56 ms (28.85%) | Memory: 48.7 MB (73.24%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,10 +3,11 @@
  * @return {number}
  */
 var lengthOfLastWord = function (s) {
-    s = s.trim()
     let length = s.length
     let count = 0
-    for (let i = length - 1; i >= 0; i--) {
+    let j = length - 1
+    while (s[j] === ' ') j--
+    for (let i = j; i >= 0; i--) {
         if (s[i] === " ") return count;
         count++
     }
